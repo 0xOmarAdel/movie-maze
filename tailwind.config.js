ecb5a36b6000449fcdd88c3f817d1b2d86d941ab
Heisenberg <omaradelawad20013@gmail.com>
2023-09-27 12:54:54 +0300

    added screen sized and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+      colors: {},
+    },
   },
   plugins: [],
 };
