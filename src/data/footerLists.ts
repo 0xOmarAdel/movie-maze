10cbd60b3b7a43a23e32e547a5e3988986edec90
Heisenberg <omaradelawad20013@gmail.com>
2023-09-29 14:24:50 +0300

    created footer lists data file

---
+++ src/data/footerLists.ts
@@ -0,0 +1,84 @@
+type List = {
+  id: number;
+  text: string;
+  path: string;
+}[];
+
+export const mainList: List = [
+  {
+    id: 1,
+    text: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    text: "About Us",
+    path: "/about-us",
+  },
+  {
+    id: 3,
+    text: "Movie Providers",
+    path: "/providers/movie",
+  },
+  {
+    id: 4,
+    text: "TV Providers",
+    path: "/providers/tv",
+  },
+];
+
+export const movieList: List = [
+  {
+    id: 1,
+    text: "Upcoming",
+    path: "/movies/upcoming",
+  },
+  {
+    id: 2,
+    text: "Now Playing",
+    path: "/movies/now-playing",
+  },
+  {
+    id: 3,
+    text: "Popular",
+    path: "/movies/popular",
+  },
+  {
+    id: 4,
+    text: "Top Rated",
+    path: "/movies/top-rated",
+  },
+  {
+    id: 5,
+    text: "Trending",
+    path: "/movies/trending",
+  },
+];
+
+export const seriesList: List = [
+  {
+    id: 1,
+    text: "Airing Today",
+    path: "/tv-series/airing-today",
+  },
+  {
+    id: 2,
+    text: "On the Air",
+    path: "/tv-series/on-the-air",
+  },
+  {
+    id: 3,
+    text: "Popular",
+    path: "/tv-series/popular",
+  },
+  {
+    id: 4,
+    text: "Top Rated",
+    path: "/tv-series/top-rated",
+  },
+  {
+    id: 5,
+    text: "Trending",
+    path: "/tv-series/trending",
+  },
+];
