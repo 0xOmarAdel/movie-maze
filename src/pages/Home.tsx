a38e59393080c943b8ce367146950ea289152acd
Heisenberg <omaradelawad20013@gmail.com>
2023-09-29 16:26:24 +0300

    added more lists to the Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,20 @@
 import HeroSlider from "../components/HeroSlider";
+import SwiperList from "../components/SwiperList/SwiperList.js";
 
 const Home = () => {
   return (
     <>
       <HeroSlider />
+      <div className="py-16 px-24">
+        <SwiperList title="Upcoming Movies" category="movie" type="upcoming" />
+        <SwiperList
+          title="TV Series Airing Today"
+          category="tv"
+          type="airing_today"
+        />
+        <SwiperList title="Popular TV Series" category="tv" type="popular" />
+        <SwiperList title="Popular Movies" category="movie" type="popular" />
+      </div>
     </>
   );
 };
