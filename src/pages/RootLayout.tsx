78fae1b229f431ba64ee00182d86a564223cae83
Heisenberg <omaradelawad20013@gmail.com>
2023-09-29 14:33:19 +0300

    added the footer to the RootLayout page

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../layout/Header";
+import Footer from "../layout/Footer";
 
 const RootLayout = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 };
