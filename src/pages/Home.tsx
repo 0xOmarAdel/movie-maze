d55b6e40cad677271488e11c085b6d6a9a182fef
Heisenberg <omaradelawad20013@gmail.com>
2023-09-29 16:33:27 +0300

    added links to the lists in the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,15 +5,31 @@ const Home = () => {
   return (
     <>
       <HeroSlider />
-      <div className="py-16 px-24">
-        <SwiperList title="Upcoming Movies" category="movie" type="upcoming" />
+      <div className="py-16 px-24 flex flex-col gap-5">
+        <SwiperList
+          title="Upcoming Movies"
+          link="/movies/upcoming"
+          category="movie"
+          type="upcoming"
+        />
         <SwiperList
           title="TV Series Airing Today"
+          link="/tv-series/airing-today"
           category="tv"
           type="airing_today"
         />
-        <SwiperList title="Popular TV Series" category="tv" type="popular" />
-        <SwiperList title="Popular Movies" category="movie" type="popular" />
+        <SwiperList
+          title="Popular TV Series"
+          link="/tv-series/popular"
+          category="tv"
+          type="popular"
+        />
+        <SwiperList
+          title="Popular Movies"
+          link="/movies/popular"
+          category="movie"
+          type="popular"
+        />
       </div>
     </>
   );
