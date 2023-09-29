ed3325d80fdda73c9112582aaa8edc0580b70fa7
Heisenberg <omaradelawad20013@gmail.com>
2023-09-29 08:09:51 +0300

    added routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
