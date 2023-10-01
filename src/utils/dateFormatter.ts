b5413a4102d04a09f989af7379b98be55f937860
Heisenberg <omaradelawad20013@gmail.com>
2023-10-01 20:49:37 +0300

    created date and time formatter utils functions

---
+++ src/utils/dateFormatter.ts
@@ -0,0 +1,12 @@
+import { DateTimeFormatOptions } from "intl";
+
+export const dateFormatter = (inputString: string) => {
+  const inputDate = new Date(inputString);
+  const options: DateTimeFormatOptions = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+  const output = inputDate.toLocaleDateString("en-US", options);
+  return output;
+};
---
+++ src/utils/timeFormatter.ts
@@ -0,0 +1,7 @@
+export const timeFormatter = (inputMinutes: number) => {
+  const hours = Math.floor(inputMinutes / 60);
+  const minutes = inputMinutes % 60;
+  const output = `${hours}h ${minutes}m`;
+
+  return output;
+};
