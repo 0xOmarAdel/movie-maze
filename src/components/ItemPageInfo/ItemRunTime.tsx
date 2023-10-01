a5becc9d16943a82205b8016726638fa293bc44c
Heisenberg <omaradelawad20013@gmail.com>
2023-10-01 21:01:46 +0300

    created an ItemRunTime component

---
+++ src/components/ItemPageInfo/ItemRunTime.tsx
@@ -0,0 +1,16 @@
+import { timeFormatter } from "../../utils/timeFormatter.ts";
+
+type Props = {
+  runtime: number;
+};
+
+const ItemRunTime: React.FC<Props> = ({ runtime }) => {
+  return (
+    <div className="flex flex-row gap-2">
+      <p className="text-lg text-white font-medium">Runtime:</p>
+      <p className="text-lg text-gray-300">{timeFormatter(runtime)}</p>
+    </div>
+  );
+};
+
+export default ItemRunTime;
