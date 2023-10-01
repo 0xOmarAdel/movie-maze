812b720ae524661a92f2f858f2aff9829ad2bd77
Heisenberg <omaradelawad20013@gmail.com>
2023-10-01 18:37:05 +0300

    created a ItemRating component

---
+++ src/components/ItemPageInfo/ItemRating.tsx
@@ -0,0 +1,32 @@
+import {
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from "react-circular-progressbar";
+
+type Props = {
+  rating: number;
+};
+
+const ItemRating: React.FC<Props> = ({ rating }) => {
+  const firstNumberAfterDot = Math.floor(rating * 10) % 10;
+  const output = firstNumberAfterDot
+    ? parseFloat(rating.toFixed(1))
+    : parseFloat(rating.toFixed(1)) + ".0";
+
+  return (
+    <div className="w-16 h-16">
+      <CircularProgressbarWithChildren
+        styles={buildStyles({
+          pathTransitionDuration: 0.5,
+          pathColor: `#6366f1`,
+          trailColor: "#d6d6d6",
+        })}
+        value={50}
+      >
+        <p className="text-xl text-white font-medium">{output}</p>
+      </CircularProgressbarWithChildren>
+    </div>
+  );
+};
+
+export default ItemRating;
