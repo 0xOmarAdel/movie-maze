d7af823e39c9ea9d08865e904c1a0a64ad491509
Heisenberg <omaradelawad20013@gmail.com>
2023-10-02 00:01:23 +0300

    fixed text on smaller screens

--- src/components/ItemPageInfo/ItemReleaseDate.tsx
+++ src/components/ItemPageInfo/ItemReleaseDate.tsx
@@ -7,8 +7,12 @@ type Props = {
 const ItemReleaseDate: React.FC<Props> = ({ date }) => {
   return (
     <div className="flex flex-row gap-2">
-      <p className="text-lg text-white font-medium">Release Date:</p>
-      <p className="text-lg text-gray-300">{dateFormatter(date)}</p>
+      <p className="text-lg text-white font-medium whitespace-nowrap">
+        Release Date:
+      </p>
+      <p className="text-lg text-gray-300 whitespace-nowrap">
+        {dateFormatter(date)}
+      </p>
     </div>
   );
 };
--- src/components/ItemPageInfo/ItemRunTime.tsx
+++ src/components/ItemPageInfo/ItemRunTime.tsx
@@ -8,7 +8,9 @@ const ItemRunTime: React.FC<Props> = ({ runtime }) => {
   return (
     <div className="flex flex-row gap-2">
       <p className="text-lg text-white font-medium">Runtime:</p>
-      <p className="text-lg text-gray-300">{timeFormatter(runtime)}</p>
+      <p className="text-lg text-gray-300 whitespace-nowrap">
+        {timeFormatter(runtime)}
+      </p>
     </div>
   );
 };
