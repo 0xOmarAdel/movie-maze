1862baffc244f58ffb486812e9248afc4213c423
Heisenberg <omaradelawad20013@gmail.com>
2023-10-01 20:56:52 +0300

    created an ItemOverview component

---
+++ src/components/ItemPageInfo/ItemOverview.tsx
@@ -0,0 +1,14 @@
+type Props = {
+  overview: string;
+};
+
+const ItemOverview: React.FC<Props> = ({ overview }) => {
+  return (
+    <div className="flex flex-col gap-3">
+      <h2 className="text-3xl text-white">Overview</h2>
+      <p className="text-lg text-gray-300">{overview}</p>
+    </div>
+  );
+};
+
+export default ItemOverview;
