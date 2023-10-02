9e49d670e4507ae8193094c7d8ee87926a79c15e
Heisenberg <omaradelawad20013@gmail.com>
2023-10-03 02:16:28 +0300

    header responsiveness

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,26 +1,55 @@
-import { Link, NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { FaPlay } from "react-icons/fa";
 import { headerLinks } from "../data/headerLists";
 
 const Header = () => {
+  const { pathname } = useLocation();
+  const [headerClasses, setHeaderClasses] = useState("");
+
+  const active = headerLinks.findIndex((e) => e.path === pathname);
+
+  useEffect(() => {
+    const shrinkHeader = () => {
+      if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+      ) {
+        setHeaderClasses("!py-5 bg-opacity-100");
+      } else {
+        setHeaderClasses("");
+      }
+    };
+    window.addEventListener("scroll", shrinkHeader);
+    return () => {
+      window.removeEventListener("scroll", shrinkHeader);
+    };
+  }, []);
+
   return (
-    <div className="page-container fixed inset-0 z-50 h-fit flex flex-row items-center justify-between">
-      <Link to="/">
-        <div className="flex flex-row items-center gap-3 text-white text-4xl font-medium">
-          <FaPlay className="text-indigo-500 scale-105" />
-          <p>Movie Maze</p>
-        </div>
-      </Link>
-      <ul className="flex flex-row gap-6 text-white text-2xl font-semibold">
-        {headerLinks.map((headerLink) => (
-          <li key={headerLink.id}>
-            <NavLink to={headerLink.path} className="relative group">
-              {headerLink.text}
-              <div className="absolute -bottom-1.5 left-1/2 w-0 h-1 bg-indigo-600 -translate-x-1/2 transition-all duration-300 group-hover:w-full"></div>
-            </NavLink>
-          </li>
-        ))}
-      </ul>
+    <div
+      className={`page-container py-9 fixed top-0 left-0 z-[100] w-full bg-[#0f0f0f] bg-opacity-0 text-white transition-all duration-300 ${headerClasses}`}
+    >
+      <div className="h-full flex items-center justify-center md:justify-between">
+        <Link to="/">
+          <div className="flex flex-row items-center gap-3 text-white text-4xl font-medium">
+            <FaPlay className="text-indigo-500 scale-105" />
+            <p>Movie Maze</p>
+          </div>
+        </Link>
+        <ul className="page-container !py-5 md:!p-0 fixed md:static bottom-0 left-0 w-full md:w-fit flex items-center justify-between gap-8 bg-[#0f0f0f] md:bg-transparent header__nav">
+          {headerLinks.map((e, i) => (
+            <li
+              key={i}
+              className={`relative text-2xl font-bold ${
+                i === active ? "active" : ""
+              }`}
+            >
+              <Link to={e.path}>{e.text}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
