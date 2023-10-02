a66d23f4cc2f31af9e5b23ecab12ef7752b07f3c
Heisenberg <omaradelawad20013@gmail.com>
2023-10-03 02:28:01 +0300

    decreased the gap in the ItemOverview component

--- src/components/ItemPageInfo/ItemOverview.tsx
+++ src/components/ItemPageInfo/ItemOverview.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 const ItemOverview: React.FC<Props> = ({ overview }) => {
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col gap-2">
       <h2 className="text-3xl text-white">Overview</h2>
       <p className="text-lg text-gray-300">{overview}</p>
     </div>
