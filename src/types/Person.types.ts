7dbea5c2ae7b868abb23f018cd581dcb7b7ef9c4
Heisenberg <omaradelawad20013@gmail.com>
2023-10-03 13:45:27 +0300

    created a Person type

---
+++ src/types/Person.types.ts
@@ -0,0 +1,13 @@
+export type PersonType = {
+  adult: boolean;
+  character: string;
+  credit_id: string;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  order: 0;
+  original_name: string;
+  popularity: 43.49;
+  profile_path: string;
+};
