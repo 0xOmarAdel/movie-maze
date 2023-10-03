fe80c1a0e9b51b147eb4ed682b6dc8e2f193d216
Heisenberg <omaradelawad20013@gmail.com>
2023-10-04 02:07:10 +0300

    added toasts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import RootLayout from "./pages/RootLayout";
 import ErrorPage from "./pages/ErrorPage";
 import Home from "./pages/Home";
@@ -21,6 +24,19 @@ const App: React.FC = () => {
           <Route path="/providers/:category" element={<Providers />} />
         </Route>
       </Routes>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        progressStyle={{ backgroundColor: "#6366f1" }}
+      />
     </>
   );
 };
