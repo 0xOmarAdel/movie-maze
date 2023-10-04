6310b0ede24c4e63d2086e97b15f349820fa18b7
Heisenberg <omaradelawad20013@gmail.com>
2023-10-04 19:15:42 +0300

    fixed an error ig

--- src/api/apiKey.ts
+++ src/api/apiKey.ts
@@ -1,1 +1,3 @@
-export const apiKey = import.meta.env.VITE_API_KEY || process.env.API_KEY;
+import * as process from "process";
+
+export const apiKey = process.env?.API_KEY || import.meta.env?.VITE_API_KEY;
