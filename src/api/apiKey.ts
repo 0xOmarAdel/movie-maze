f819c8b05bd74556d8c63962f7b952b2d9ee7958
Heisenberg <omaradelawad20013@gmail.com>
2023-10-04 19:30:21 +0300

    testing env var

--- src/api/apiKey.ts
+++ src/api/apiKey.ts
@@ -1,1 +1,4 @@
-export const apiKey = import.meta.env.VITE_API_KEY;
+import * as process from "process";
+console.log(process.env);
+// export const apiKey = import.meta.env.VITE_API_KEY;
+export const apiKey = process.env.API_KEY;
