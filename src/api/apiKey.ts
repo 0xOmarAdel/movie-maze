1a73a31abec2aa92db377246f58192f66a66a15e
Heisenberg <omaradelawad20013@gmail.com>
2023-10-04 19:34:40 +0300

    .

--- src/api/apiKey.ts
+++ src/api/apiKey.ts
@@ -1,4 +1,5 @@
 import * as process from "process";
 console.log(process.env);
+console.log(import.meta.env);
 // export const apiKey = import.meta.env.VITE_API_KEY;
 export const apiKey = process.env.API_KEY;
